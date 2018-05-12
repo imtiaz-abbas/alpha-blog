994c1cef9c3ff58e73976552b9124fa159078473
Imtiaz Abbas <imtiaz1919@gmail.com>
2018-05-12 19:01:47 +0530

    edit view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,18 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
-
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def update
+    @article = Article.find(params[:id])
+    if @article.update article_params
+      flash[:notice] = "Article sucessfully updated."
+      redirect_to article_path
+    else
+      render 'edit'
+    end
+  end
   def create
     # render plain: params[:article].inspect
     @article = Article.new(article_params)
