55583d7c881a803df2aed00fdc6e83630355ad1f
Imtiaz Abbas <imtiaz1919@gmail.com>
2018-05-13 23:21:45 +0530

    email validation error rectified

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
   length: {maximum: 30, minimum: 5}
   VALID_EMAIL_REGEX= /\a[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,
-  length:{maximum: 100},
-  uniqueness: {case_sensitive: false},
-  format: {with: VALID_EMAIL_REGEX}
+  length: {maximum: 100},
+  uniqueness: {case_sensitive: false}
 end
