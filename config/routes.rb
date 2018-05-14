c55cd7a16e9090bb80fa9271917e71560207006e
Imtiaz Abbas <imtiaz1919@gmail.com>
2018-05-14 14:51:57 +0530

    added login and logout paths and controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'signup', to: 'users#new'
   get 'about' , to: 'pages#about'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   resources :users, except: [:new]
   resources :articles
 end
